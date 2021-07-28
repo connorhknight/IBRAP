d346063f1ecedd66db2d1bf037502e741cb9b4cd
Connor Knight <connorhknight@gmail.com>
2021-07-28 14:01:35 +0100

    update

---
+++ R/plot.benchmarking.R
@@ -0,0 +1,104 @@
+#' @name plot.benchmarking
+#' @aliases plot.benchmarking
+#' 
+#' @param object An IBRAP S4 class object
+#' @param assay Character. Which assay within the object to access
+#' 
+#' @export
+
+plot.benchmarking <- function(object, 
+                              assay,
+                              clustering, 
+                              ARI){
+  
+  if(!is(object, 'IBRAP')) {
+    
+    stop('object must be of class IBRAP \n')
+    
+  }
+  
+  if(!is.character(assay)) {
+    
+    stop('assay must be character string \n')
+    
+  } else if (is.character(assay)) {
+    
+    if(!assay %in% names(object@methods)) {
+      
+      stop('assay is not contained within object@methods \n')
+      
+    }
+    
+  }
+  
+  if(!is.vector(clustering)) {
+    
+    stop('clustering must be a vector \n')
+    
+  } else {
+    
+    if(length(clustering) != ncol(object)) {
+      
+      stop('clustering must be the same length as the number of cells \n')
+      
+    }
+    
+  }
+  
+  if(!is.logical(ARI)){
+    
+    stop('ARI must be boolean. TRUE/FALSE \n')
+    
+  }
+  
+  ggarrange.tmp <- function(...) {
+    
+    egg::ggarrange(...)
+    
+  }
+  
+  clust.bench <- object@methods[[assay]]@benchmark_results[[clustering]]
+  clust.bench <- as.data.frame(clust.bench)
+
+  clust.bench[,'cluster_index'] <- rownames(clust.bench)
+  
+  if(ARI == TRUE) {
+    
+    labels <- c('ASW', 'Dunn_index', 'Connectivity', 'ARI', 'NMI', 'cluster_index')
+    
+  } else {
+    
+    labels <- c('ASW', 'Dunn_index', 'Connectivity', 'cluster_index')
+    
+  }
+  
+  colnames(clust.bench) <- labels
+
+  list.plot <- list()
+  
+  for(o in 1:sum(length(labels)-2)) {
+    
+    label <- labels[as.numeric(o)]
+
+    fig <- ggplot2::ggplot(clust.bench, ggplot2::aes_string(x = 'cluster_index', y = as.character(label), group = 1)) +
+      ggplot2::geom_point() +
+      ggplot2::geom_line() + 
+      ggplot2::theme_bw() +
+      ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, vjust = 0.5, hjust=1))
+    
+    list.plot[[as.numeric(o)]] <- fig
+    
+  }
+  
+  last.label <- labels[as.numeric(sum(length(labels)-1))]
+  
+  last.fig <- ggplot2::ggplot(clust.bench, ggplot2::aes_string(x = 'cluster_index', y = as.character(last.label), group = 1)) +
+    ggplot2::geom_point() +
+    ggplot2::geom_line() +
+    ggplot2::theme_bw() +
+    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, vjust = 0.5, hjust=1))
+  
+  list.plot[[as.numeric(sum(length(labels)-1))]] <- last.fig
+
+  do.call('ggarrange.tmp', c(plots = list.plot, ncol = 5))
+}
