e71ba24d86066651bbca2a162745d60d366fe29c
Connor Knight <connorhknight@gmail.com>
2021-08-15 21:11:37 +0100

    benchmark_integration

---
+++ R/benchmark.intergation.R
@@ -0,0 +1,161 @@
+#' @name benchmark.intergation
+#' @aliases benchmark.intergation
+#' 
+#' @title Benchmarks the cluster assignmentws
+#'
+#' @description Adopting scones method of Average Silhouette Width determination between batches. A depleted value is superior. 
+#' 
+#' @param object IBRAP S4 class object
+#' @param batch Character. Whcih column in the metadata dataframe contains the batches.
+#' @param assay Character. String containing indicating which assay to use
+#' @param reduction Character. Which reduction(s) should be used to observe integration.
+#' @param result.names Character. A vector of names for the benchmarking results. 
+#' @param n.components Numerical. How many components of the reduced embeddings should be used. Default = 2
+#' 
+#' @return Benchmarking scores for the supplied integrations
+#'
+#' @export
+
+benchmark.intergation <- function(object, batch, assays, reduction, result.names, n.components = 2) {
+  
+    
+    stop('object must be of class IBRAP \n')
+    
+  }
+  
+  if(!is.character(batch)) {
+    
+    stop('batch must be character string \n')
+    
+  } else {
+    
+    if(!batch %in% colnames(object@sample_metadata)) {
+      
+      stop('batch cannot be found in object@sample_metadata \n')
+      
+    }
+    
+  }
+  
+  if(!is.character(assays)) {
+    
+    stop('batch must be character string \n')
+    
+  } else {
+    
+    for(x in assays) {
+      
+      if(!x %in% names(object@methods)) {
+        
+        stop(paste0(x, ' cannot be found in object@methods \n'))
+        
+      }
+      
+    }
+    
+  }
+  
+  if(!is.numeric(n.components)) {
+    
+    stop('n.components must be numerical \n')
+    
+  }
+  
+  if(!is.character(result.names)) {
+    
+    stop('result.names must be charcter string(s) \n')
+    
+  } else {
+    
+    if(length(result.names) != length(reduction)) {
+      
+      stop('not enough result.names provided \n')
+      
+    }
+    
+  }
+  
+  for(p in assays) {
+    
+    reduction.list <- list()
+    red.names <- c(names(object@methods[[p]]@computational_reductions), 
+                   names(object@methods[[p]]@integration_reductions),
+                   names(object@methods[[p]]@visualisation_reductions))
+    
+    for(i in red.names) {
+      
+      if(i %in% names(object@methods[[p]]@computational_reductions)) {
+        
+        reduction.list[[i]] <- object@methods[[p]]@computational_reductions[[i]]
+        
+      }
+      
+      if(i %in% names(object@methods[[p]]@integration_reductions)) {
+        
+        reduction.list[[i]] <- object@methods[[p]]@integration_reductions[[i]]
+        
+      }
+      
+      if(i %in% names(object@methods[[p]]@visualisation_reductions)) {
+        
+        reduction.list[[i]] <- object@methods[[p]]@visualisation_reductions[[i]]
+        
+      }
+      
+    }
+    
+    if(!is.null(reduction)) {
+      
+      for(r in reduction) {
+        
+        if(!r %in% names(reduction.list)) {
+          
+          stop('reductions could not be found\n')
+          
+        }
+        
+      }
+      
+    }
+    
+    counter <- 1
+    
+    for(t in reduction) {
+      
+      cat(crayon::cyan(paste0(Sys.time(), ': benchmarking assay: ', p, ', reduction: ', t, '\n')))
+      
+      dist <- as.matrix(dist(reduction.list[[t]][, seq_len(n.components)]))
+      
+      if(isTRUE(anyNA(as.numeric(pancreas@sample_metadata[,batch])))) {
+        
+        count <- 1
+        
+        batch_tmp <- pancreas@sample_metadata[,batch]
+        
+        for(d in unique(pancreas@sample_metadata[,batch])) {
+          
+          batch_tmp[batch_tmp==d] <- count
+          
+          count <- count + 1
+          
+        }
+        
+        batch_tmp <- as.numeric(batch_tmp)
+        
+      } else {
+        
+        batch_tmp <- as.numeric(pancreas@sample_metadata[,batch])
+        
+      }
+
+      object@methods[[p]]@benchmark_results$batches[[paste0(p, '_', result.names[counter])]] <- summary(cluster::silhouette(as.numeric(batches), dist))$avg.width
+      
+      counter <- counter + 1
+      
+    }
+    
+  }
+  
+  return(object)
+  
+}
