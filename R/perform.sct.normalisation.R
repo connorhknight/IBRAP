9fb2d4a4990ac8ccb1400e855257d3eae2885b83
Connor Knight <connorhknight@gmail.com>
2022-05-25 16:10:06 +0100

    Update perform.sct.normalisation.R

--- R/perform.sct.normalisation.R
+++ R/perform.sct.normalisation.R
@@ -91,7 +91,24 @@ perform.sct <- function(object,
     
   }
   
-  seuratobj <- suppressWarnings(Seurat::CreateSeuratObject(counts = as.matrix(object@methods[[assay]][[slot]]), project = 'NA'))
+  as_matrix <- function(mat){
+    
+    tmp <- matrix(data=0L, nrow = mat@Dim[1], ncol = mat@Dim[2])
+    
+    row_pos <- mat@i+1
+    col_pos <- findInterval(seq(mat@x)-1,mat@p[-1])+1
+    val <- mat@x
+    
+    for (i in seq_along(val)){
+      tmp[row_pos[i],col_pos[i]] <- val[i]
+    }
+    
+    row.names(tmp) <- mat@Dimnames[[1]]
+    colnames(tmp) <- mat@Dimnames[[2]]
+    return(tmp)
+  }
+  
+  seuratobj <- suppressWarnings(Seurat::CreateSeuratObject(counts = as_matrix(object@methods[[assay]][[slot]]), project = 'NA'))
   seuratobj@meta.data <- cbind(seuratobj@meta.data, object@sample_metadata)
   
   start_time <- Sys.time()
