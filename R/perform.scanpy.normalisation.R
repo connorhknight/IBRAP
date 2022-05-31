86e98493e673ead67b5535c1aabc7e435858a2ae
Connor Knight <connorhknight@gmail.com>
2022-05-31 17:22:23 +0100

    Update perform.scanpy.normalisation.R

--- R/perform.scanpy.normalisation.R
+++ R/perform.scanpy.normalisation.R
@@ -201,19 +201,19 @@ perform.scanpy <- function(object,
   }
   
   set.seed(seed = seed, kind = "Mersenne-Twister", normal.kind = "Inversion")
-  print('.')
+  
   reticulate::py_set_seed(seed, disable_hash_randomization = TRUE)
-  print('.')
+  
   sc <- reticulate::import('scanpy')
-  print('.')
+  
   pd <- reticulate::import('pandas')
-  print('.')
+  
   scobj <- sc$AnnData(X = as(as_matrix_transpose(object@methods[[assay]][[slot]]), 'dgCMatrix'))
-  print('.')
+  
   scobj$obs_names <- as.factor(colnames(object@methods[[assay]][[slot]]))
-  print('.')
+  
   scobj$var_names <- as.factor(rownames(object@methods[[assay]][[slot]]))
-  print('.')
+  
   if(length(names(object@sample_metadata)) >= 1) {
     scobj$obs <- object@sample_metadata
   }
@@ -326,25 +326,25 @@ perform.scanpy <- function(object,
                                 flavor = as.character(flavor))
     
   }
-
+  print('.')
   .highly.variable.genes <- rownames(object@methods$RAW@counts)[scobj$var[['highly_variable']]]
-
+  print('.')
   scobj2 <- sc$AnnData(X = iso[scobj$var$highly_variable,])
-
+  print('.')
   scobj2$var_names <- as.factor(rownames(object@methods$RAW@counts)[scobj$var$highly_variable])
-
+  print('.')
   scobj2$obs_names <- as.factor(colnames(object@methods$RAW@counts))
-  
+  print('.')
   if(!is.null(vars.to.regress)) {
     
     if(length(vars.to.regress) > 1) {
       
       scobj2$obs <- pd$DataFrame(data = as.data.frame(object@sample_metadata[,vars.to.regress]))
-      
+      print('.')
     } else {
       
       scobj2$obs[,vars.to.regress] <- object@sample_metadata[,vars.to.regress]
-      
+      print('.')
     }
     
   }
