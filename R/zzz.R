4c5c775487871ac4fbabced471e507b2223d164c
Connor Knight <connorhknight@gmail.com>
2022-09-23 15:33:57 +0100

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -64,7 +64,7 @@
   
   ####################################################
   
-  reticulate::py_install('louvain==0.7.1', pip = F)
+  reticulate::py_install('louvain', pip = F)
   
   if(isFALSE(reticulate::py_module_available('louvain'))) {
     
