335bed799f3f3cb5c58978a300cecdca3762573c
Connor Knight <connorhknight@gmail.com>
2022-11-11 14:28:03 +0000

    update

--- R/perform.scanpy.normalisation.R
+++ R/perform.scanpy.normalisation.R
@@ -406,19 +406,19 @@ perform.scanpy <- function(object,
   
   function_time <- end_time - start_time
   
-  if(!'normalisation_method' %in% colnames(object@pipelines)) {
-    
-    object@pipelines <- data.frame(normalisation_method=paste0('SCANPY', new.assay.suffix), normalisation_time=function_time)
-    
-  } else if (paste0('SCANPY', new.assay.suffix) %in% object@pipelines[,'normalisation_method']) {
-    
-    object@pipelines[which(object@pipelines[,'normalisation_method']==paste0('SCANPY', new.assay.suffix)),] <- data.frame(normalisation_method=paste0('SCANPY', new.assay.suffix), normalisation_time=function_time)
-    
-  } else {
-    
-    object@pipelines <- rbind(object@pipelines, data.frame(normalisation_method=paste0('SCANPY', new.assay.suffix), normalisation_time=function_time))
-    
-  }
+  # if(!'normalisation_method' %in% colnames(object@pipelines)) {
+  #   
+  #   object@pipelines <- data.frame(normalisation_method=paste0('SCANPY', new.assay.suffix), normalisation_time=function_time)
+  #   
+  # } else if (paste0('SCANPY', new.assay.suffix) %in% object@pipelines[,'normalisation_method']) {
+  #   
+  #   object@pipelines[which(object@pipelines[,'normalisation_method']==paste0('SCANPY', new.assay.suffix)),] <- data.frame(normalisation_method=paste0('SCANPY', new.assay.suffix), normalisation_time=function_time)
+  #   
+  # } else {
+  #   
+  #   object@pipelines <- rbind(object@pipelines, data.frame(normalisation_method=paste0('SCANPY', new.assay.suffix), normalisation_time=function_time))
+  #   
+  # }
   
   return(object)
 }
