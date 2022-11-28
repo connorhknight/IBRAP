4ca10d7c66b8ebc46fbba815abe6a2e4c7560425
Connor Knight <c.h.knight@qmul.ac.uk>
2022-11-28 19:24:04 +0000

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -5,12 +5,3 @@
   packageStartupMessage("Welcome to IBRAP")
   
 }
-
-.onLoad <- function(libname, pkgname) {
-  
-  if(!suppressWarnings(suppressPackageStartupMessages(require('celltalker', quietly=TRUE,character.only=TRUE)))){
-    remotes::install_github("arc85/celltalker")
-    suppressPackageStartupMessages(library('celltalker',character.only=TRUE))
-  }
-  
-}
