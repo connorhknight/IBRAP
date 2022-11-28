c6480d1de67a0b80667040abecfa0de6d2e43a23
Connor Knight <c.h.knight@qmul.ac.uk>
2022-11-28 19:16:18 +0000

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -9,7 +9,7 @@
 .onLoad <- function(libname, pkgname) {
   
   if(!suppressWarnings(suppressPackageStartupMessages(require('celltalker', quietly=TRUE,character.only=TRUE)))){
-    devtools::install_github("arc85/celltalker")
+    remotes::install_github("arc85/celltalker")
     suppressPackageStartupMessages(library('celltalker',character.only=TRUE))
   }
   
